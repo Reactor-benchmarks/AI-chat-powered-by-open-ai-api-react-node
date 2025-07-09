8603bfdc55b20122d4ce2e0b3f09a1f44cc7b1c0
ANON <anon@example.com>
2025-07-09 11:24:52 -0400

    Add after_transformation_instrumented version

---
+++ src/App.js
@@ -0,0 +1,87 @@
+import { useRef } from "react";
+import { memo } from "react";
+import "./normalize.css";
+import "./App.css";
+import { useState } from "react";
+const App = memo(function App() {
+  console.log(window.globalCount++);
+  const input = useRef("");
+  const [chatlog, setchatlog] = useState([{
+    user: "gpt",
+    message: "Hello what are you bothering me today for"
+  }, {
+    user: "karim",
+    message: "do you know how to solve 3 + 3"
+  }]);
+  function clearChat() {
+    setchatlog([]);
+  }
+  async function handleSubmit(e) {
+    e.preventDefault();
+    console.log(e);
+    const chatlogNew = [...chatlog, {
+      user: "me",
+      message: `${input.current.value}`
+    }];
+    await (input.current.value = "");
+    setchatlog(chatlogNew);
+    const messages = chatlogNew.map(message => message.message).join("\n");
+    const response = await fetch("https://ai-chatbot-backend.onrender.com/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        message: messages
+      })
+    });
+    const data = await response.json();
+    await setchatlog([...chatlogNew, {
+      user: "gpt",
+      message: `${data.message}`
+    }]);
+    console.log(data.message);
+  }
+  return <>
+      <div className="App">
+        <aside className="side-menu">
+          <div className="side-menu-button" onClick={clearChat}>
+            <span>+</span>
+            New Chat
+          </div>
+        </aside>
+        <section className="chatbox" style={{
+        height: "80" + "vh",
+        overflowY: "scroll"
+      }}>
+          <div className="chat-log">
+            {chatlog.map((message, index) => {
+            console.log(window.globalCount++);
+            return <ChatMessage key={index} message={message} />;
+          })}
+          </div>
+          <div className="chat-input-holder"></div>
+        </section>
+      </div>
+      <form onSubmit={handleSubmit} style={{
+      position: "absolute",
+      top: "90" + "%",
+      left: "15" + "%",
+      width: "75" + "%"
+    }}>
+        <input className="chat-input-textarea" placeholder="Type your message here" ref={input}></input>
+      </form>
+    </>;
+});
+const ChatMessage = ({
+  message
+}) => {
+  console.log(window.globalCount++);
+  return <div className={`chat-message ${message.user == "gpt" && "chatgpt"}`}>
+      <div className="chat-message-center">
+        <div className={`avatar ${message.user == "gpt" && "chatgpt"}`}></div>
+        <div className="message">{message.message}</div>
+      </div>
+    </div>;
+};
+export default App;
